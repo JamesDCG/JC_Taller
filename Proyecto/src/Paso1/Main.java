bbd38d234ca424bb5d330172a19730ff08325986
JamesDCG <jamesdcg9@hotmail.com>
2025-05-08 17:23:17 -0500

    1

--- Proyecto/src/Paso1/Main.java
+++ Proyecto/src/Paso1/Main.java
@@ -3,27 +3,31 @@ package Paso1;
 public class Main {
     public static void main(String[] args) {
 
+        String[] ciudades = {"Quito", "Guayaquil", "Cuenca"};
+
         String[] meses = {"Enero", "Febrero", "Marzo", "Abril" , "Mayo", "Junio",
         "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
-        int [] lluvia = new int[12];
-        lluvia[0] = 23;        lluvia[1] = 8;        lluvia[2] = 12;       lluvia[3] = 13;
-        lluvia[4] = 42;        lluvia[5] = 30;       lluvia[6] = 2;        lluvia[7] = 3;
-        lluvia[8] = 33;        lluvia[9] = 22;       lluvia[10] = 11;      lluvia[11] = 7;
+        int[][] lluvia = {
+                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+                {10, 20, 15, 18, 22, 25, 30, 28, 27, 20, 19, 15},
+                {5, 6, 7, 5, 6, 4, 3, 4, 5, 6, 5, 7}
+        };
 
-        int acumulado = 0;
-
-        for (int i = 0 ; i < lluvia.length ; i++)
+        for (int ciudad = 0; ciudad < ciudades.length; ciudad++)
         {
-            System.out.println("La lluvia en " + meses[i] + " es: " + lluvia[i] + " l/m2.");
-            acumulado += lluvia[i];
+            int lluviaTotal = 0;
+
+            System.out.println("\nCiudad: " + ciudades[ciudad]);
+            for (int mes = 0; mes < meses.length; mes++)
+            {
+                System.out.println("La lluvia en " + meses[mes] + " es: " + lluvia[ciudad][mes] + " l/m2");
+                lluviaTotal += lluvia[ciudad][mes];
+            }
+
+            int promedio = lluviaTotal / 12;
+            System.out.println("La lluvia total en " + ciudades[ciudad] + " es: " + lluviaTotal + " l/m2");
+            System.out.println("El promedio de la lluvia en " + ciudades[ciudad] + " es: " + promedio + " l/m2");
         }
-
-        System.out.println("La lluvia acumulada es: " + acumulado);
-        System.out.println("La lluvia promedio es: " + acumulado/12);
-
-        for(String nombreMes : meses)
-            System.out.println(nombreMes);
-
     }
 }
