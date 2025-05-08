742b14c444b4956d59795ed90646878fb43ed0ab
JamesDCG <jamesdcg9@hotmail.com>
2025-05-08 17:03:36 -0500

    1

--- Proyecto/src/Paso1/Main.java
+++ Proyecto/src/Paso1/Main.java
@@ -20,6 +20,7 @@ public class Main {
         }
 
         System.out.println("La lluvia acumulada es: " + acumulado);
+        System.out.println("La lluvia promedio es: " + acumulado/12);
 
     }
 }
