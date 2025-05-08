843e60ba1cafd3f04d47a13904a6c6176d059a4a
JamesDCG <jamesdcg9@hotmail.com>
2025-05-08 16:38:28 -0500

    1

--- Proyecto/src/Paso1/Main.java
+++ Proyecto/src/Paso1/Main.java
@@ -3,7 +3,24 @@ package Paso1;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+        String[] meses = {"Enero", "Febrero", "Marzo", "Abril" , "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+        int [] lluvia = new int[12];
+        lluvia[0] = 23;
+        lluvia[1] = 8;
+        lluvia[2] = 12;
+        lluvia[3] = 13;
+        lluvia[4] = 42;
+        lluvia[5] = 30;
+        lluvia[6] = 2;
+        lluvia[7] = 3;
+        lluvia[8] = 33;
+        lluvia[9] = 22;
+        lluvia[10] = 11;
+        lluvia[11] = 7;
+
+
 
     }
 }
