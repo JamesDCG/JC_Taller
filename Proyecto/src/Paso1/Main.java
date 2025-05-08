dc2fdbeeb87f6946cafe125231ea3f02ebf31ddd
JamesDCG <jamesdcg9@hotmail.com>
2025-05-08 16:56:04 -0500

    1

--- Proyecto/src/Paso1/Main.java
+++ Proyecto/src/Paso1/Main.java
@@ -7,20 +7,19 @@ public class Main {
         "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
         int [] lluvia = new int[12];
-        lluvia[0] = 23;
-        lluvia[1] = 8;
-        lluvia[2] = 12;
-        lluvia[3] = 13;
-        lluvia[4] = 42;
-        lluvia[5] = 30;
-        lluvia[6] = 2;
-        lluvia[7] = 3;
-        lluvia[8] = 33;
-        lluvia[9] = 22;
-        lluvia[10] = 11;
-        lluvia[11] = 7;
+        lluvia[0] = 23;        lluvia[1] = 8;        lluvia[2] = 12;       lluvia[3] = 13;
+        lluvia[4] = 42;        lluvia[5] = 30;       lluvia[6] = 2;        lluvia[7] = 3;
+        lluvia[8] = 33;        lluvia[9] = 22;       lluvia[10] = 11;      lluvia[11] = 7;
 
+        int acumulado = 0;
 
+        for (int i = 0 ; i < lluvia.length ; i++)
+        {
+            System.out.println("La lluvia en " + meses[i] + " es: " + lluvia[i] + " l/m2.");
+            acumulado += lluvia[i];
+        }
+
+        System.out.println("La lluvia acumulada es: " + acumulado);
 
     }
 }
